b8833094909452dbad163a66db3be96e672767c6
gavin <meiyan532484710@163.com>
2019-12-06 14:24:54 +0800

    我的rust-web
    Signed-off-by: gavin <meiyan532484710@163.com>

---
+++ src/main.rs
@@ -0,0 +1,60 @@
+
+/*
+TODO Rust中的独立编译单位是crate：rustc每次编译一个crate，生成一个库或者可执行文件
+*/
+extern crate myweb; // TODO 通过 extern crate 引入的 crate 或者类库
+
+/*
+TODO 在使用 extern 引入包之后，可以用 use 将模块引入到当前作用域
+*/
+
+use myweb::ThreadPool; // 引用 lib.rs 中的
+
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::fs::File;
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming().take(2) {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+
+    println!("Shutting down.");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+
+    let mut file = File::open(filename).unwrap();
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
